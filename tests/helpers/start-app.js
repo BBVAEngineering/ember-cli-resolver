0de194e497b088bf0625d342b0fe9284da21ecb9
Beatriz de Miguel <beatriz.demiguel@bbva.com>
2017-06-01 11:34:20 +0200

    use object assign

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -2,13 +2,15 @@ import Ember from 'ember';
 import Application from '../../app';
 import config from '../../config/environment';
 
+const { run } = Ember;
+
 export default function startApp(attrs) {
-	let attributes = Ember.merge({}, config.APP);
+	let attributes = Object.assign({}, config.APP);
 
-	attributes = Ember.merge(attributes, attrs); // use defaults, but you can override;
+	attributes = Object.assign(attributes, attrs); // use defaults, but you can override;
 
-	return Ember.run(() => {
-		let application = Application.create(attributes);
+	return run(() => {
+		const application = Application.create(attributes);
 
 		application.setupForTesting();
 		application.injectTestHelpers();
